3dd5084f9e5a7f8d7b1329d35a1776a4ae7318b1
ParvN <parvathyn8@gmail.com>
2019-04-02 15:24:37 +0200

    Added Test functions

--- test_function.py
+++ test_function.py
@@ -15,3 +15,29 @@ test_case_1 = [([0, 1, 2, 1, 2, 1, 0],[2,4]),([-i**2 for i in range(-3, 4)],[3])
 def test_case_1_maxima(inp,exp):
     out = find_maxima(inp)
     assert out ==exp
+
+def test_randomized():
+    #given
+    seedval =0
+    rand_gen = np.random.RandomState(seed = seedval)
+    numel = rand_gen.randint(0,1000)
+    test_vec = rand_gen.random_integers(low=1,high=20,size=numel)
+    print(f'test_vec:{test_vec}')
+    
+    #When
+    out =find_maxima(test_vec)
+    
+    #Then
+    if out[0]==0:
+        assert test_vec[0] > test_vec[1]
+    else:
+        for k in range(1,out[0]):
+            assert test_vec[k]>=test_vec[k-1]
+    if len(out)>3 :
+        up=False
+        for i,j in zip(out[1:-2],out[2:-1]):
+            for k in range(i,j):
+                if test_vec[k] >test_vec[k-1]:
+                    up = True
+                if test_vec[k] < test_vec[k-1]:
+                    assert not up
